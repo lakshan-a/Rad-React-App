b6409d14703fb4a9494e771c4f467a63ceeefa86
lakshan <lakshanrashmika16302@gmail.com>
2024-06-01 14:04:11 +0530

    updated

--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import { Carousel } from "flowbite-react";
 import Card from '../Card/Card';
+import { promises } from 'fs';
 
 
 
@@ -9,8 +10,24 @@ export default class Home extends Component {
   constructor(props:{}){
     super(props);
     this.state={
-      
+
     }
+  }
+
+  componentDidMount(): void {
+    this.fetchData();
+  }
+
+  fetchData =async () =>{
+    try{
+
+     let response = await fetch('./public/product-data.json');
+     let jsonData = await response.json();
+
+    }catch (error){
+      console.error('Erroe Fetching Data', error);
+    }
+
   }
 
   render() {
